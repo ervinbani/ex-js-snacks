3a5f0d78b617d4f00ca43c4fed2bcf64455351ac
ervinbani <bani.ervin@gmail.com>
2018-03-21 16:51:33 +0100

    peso

--- main.js
+++ main.js
@@ -1,53 +1,53 @@
 //oggetto zucchine, peso in gr, lunghezza in cm
 var zucchina1 = {
   "varietà" : "Bolognese",
-  "peso" : "90",
-  "lunghezza" : "15"
+  "peso" : 90,
+  "lunghezza" : 15
 };
 var zucchina2 = {
   "varietà" : " Piacenza",
-  "peso" : "80",
-  "lunghezza" : "13"
+  "peso" : 80,
+  "lunghezza" : 13
 };
 var zucchina3 = {
   "varietà" : " Piacenza",
-  "peso" : "80",
-  "lunghezza" : "13"
+  "peso" : 80,
+  "lunghezza" : 13
 };
 var zucchina4 = {
   "varietà" : " varietà4",
-  "peso" : "82",
-  "lunghezza" : "12"
+  "peso" : 82,
+  "lunghezza" : 12
 };
 var zucchina5 = {
   "varietà" : " varietà5",
-  "peso" : "78",
-  "lunghezza" : "16"
+  "peso" : 78,
+  "lunghezza" : 16
 };
 var zucchina6 = {
   "varietà" : " varietà6",
-  "peso" : "77",
-  "lunghezza" : "17"
+  "peso" : 77,
+  "lunghezza" : 17
 };
 var zucchina7 = {
   "varietà" : " varietà7",
-  "peso" : "75",
-  "lunghezza" : "10"
+  "peso" : 75,
+  "lunghezza" : 10
 };
 var zucchina8 = {
   "varietà" : " varietà5",
-  "peso" : "71",
-  "lunghezza" : "10"
+  "peso" : 71,
+  "lunghezza" : 10
 };
 var zucchina9 = {
   "varietà" : " varietà9",
-  "peso" : "65",
-  "lunghezza" : "13"
+  "peso" : 65,
+  "lunghezza" : 13
 };
 var zucchina10 = {
   "varietà" : " varietà10",
-  "peso" : "66",
-  "lunghezza" : "12"
+  "peso" : 66,
+  "lunghezza" : 12
 };
 
 //array di oggetti
@@ -58,5 +58,6 @@ sommaPeso=0;
 for(var i=0; i<arrayZucchina.length; i++){
   sommaPeso=sommaPeso+arrayZucchina[i].peso;
 }
-
-document.write("peso totale =  ", + sommaPeso)
+somma=zucchina1.peso+zucchina2.peso+zucchina3.peso+zucchina4.peso+zucchina5.peso+zucchina6.peso+zucchina7.peso+zucchina8.peso+zucchina9.peso+zucchina10.peso
+document.write("peso totale =  ", + sommaPeso);
+document.write("peso totale =  ", + somma);
