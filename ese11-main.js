744383d164ca0835c98f7bf2b958c9cad3ab9991
ervinbani <bani.ervin@gmail.com>
2018-03-22 15:58:36 +0100

    ese11

--- ese11-main.js
+++ ese11-main.js
@@ -1,10 +1,13 @@
 var array1=[1, 2, 1, 3];
 var array2=["a", "b", "e", "d"];
+
+
+function mixedarray(){
 var mixedarr=[];
-
-
 for(var i=0;i<array1.length; i++){
   mixedarr.push(array1[i], array2[i]);
 
+}
+return mixedarr;
 }
 console.log(mixedarr);
